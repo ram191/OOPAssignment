28f9693dc7eadc754be8c4890005052dff55718c
Rayhan <training@Gigamings-iMac.local>
2020-02-07 19:19:31 +0700

    Added update

--- OopAssignment/CartClass.cs
+++ OopAssignment/CartClass.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace OopAssignment
 {
-    public class CartClass
+    public class Cart
     {
-        public CartClass()
+        public static IItems obj = new IItems();
+        public static List<Cart> itemCart = new List<Cart>();
+
+        public Cart AddItem(int id, int price, int qty = 1)
         {
+            obj.Item_id = id;
+            obj.Price = price;
+            obj.Quantity = qty;
+            itemCart.Add(this);
+            return this;
         }
+
+        public static int TotalItems()
+        {
+            return itemCart.Count();
+        }
+
+        public void TotalQuantity()
+        {
+            var x = itemCart
+                .Select(x => x.Quantity)
+                .Sum();
+        }
+    }
+
+    public class IItems
+    {
+        public int Item_id { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
     }
 }
--- OopAssignment/Program.cs
+++ OopAssignment/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace OopAssignment
 {
@@ -11,8 +13,17 @@ namespace OopAssignment
             //var decrypt = CipherClass.decrypt(message, "password");
             //Console.WriteLine(message);
 
-            Auth.Login("ram191", "123456");
-            Auth.LastLogin();
+            //Auth.Login("ram191", "123456");
+            //Auth.LastLogin();
+            //List<Cart> itemCart = new List<Cart>();
+
+            Cart newcart = new Cart();
+
+            newcart.AddItem(2, 20000, 2)
+                .AddItem(3, 30000, 3)
+                .AddItem(4, 131313, 4);
+
+            Console.WriteLine(Cart.TotalItems());
         }
     }
 
